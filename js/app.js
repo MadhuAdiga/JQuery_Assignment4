20862c4ecdb5b464623fed4ab91f648e9080782b
MadhuAdiga <madhu.vah@gmail.com>
2017-01-20 23:06:49 +0530

    Assignment4

---
+++ js/app.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){	
+$("#acadgild").attr("href", "http://www.w3schools.com/jquery/default.asp"); //Change the href to w3schools.com
+$("#acadgild").text("W3 Schools JQuery Tutorial"); //Change the text of the anchor tag
+});
